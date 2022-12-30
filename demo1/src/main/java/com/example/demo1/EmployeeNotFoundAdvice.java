314615564073004a78d9734d791209a1f4edfb9a
Saurabh Munghate <saumunghate@gmail.com>
2022-12-31 00:12:06 +0530

    5 chmaged

--- demo1/src/main/java/com/example/demo1/EmployeeNotFoundAdvice.java
+++ demo1/src/main/java/com/example/demo1/EmployeeNotFoundAdvice.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ControllerAdvice
 class EmployeeNotFoundAdvice {
+//Sau
 
   @ResponseBody
   @ExceptionHandler(EmployeeNotFoundException.class)
